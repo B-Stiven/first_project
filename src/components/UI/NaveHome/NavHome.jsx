bc4f0dbdaa7d11840e28b571e9739e101e0e5a4d
ingrid camila rodriguez <irodriguez29j04@gmail.com>
2022-05-13 16:19:05 -0500

    seccon commit

---
+++ src/components/UI/NaveHome/NavHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavHome = ({textNav, ref}) => {
+  return (
+
+    <nav>
+        <a href='#'>{textNav}</a>
+        <a href='#'>{textNav}</a>
+        <a href='#'>{textNav}</a>
+
+
+    </nav>
+  )
+}
